c9050afe0c681b7f7055e6b4f0f8674aaede7bf9
Anup Sukumaran <anup.sukumaran9@gmail.com>
2020-07-20 17:46:07 +0530

    - 5:45 pm - Fixing the read more option

--- ReadMoreText/ViewController.swift
+++ ReadMoreText/ViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import Foundation
+import SASReadMoreTxtView
 
 class ViewController: UIViewController {
     
@@ -17,6 +18,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
          tv.text = "👨‍👩‍👧‍👦👨‍👩‍👧‍👦👨‍👩‍👧‍👦👨‍👩‍👧‍👦 Lorem http://ipsum.com dolor sit er elit lamet, consectetaur cillium adipisicing pecu, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Nam liber te conscient to factor tum poen legum odioque civiuda."
+       // let ss =  UIFont.boldSystemFont(ofSize: 16)
         let readMoreTextAttributes: [NSAttributedString.Key: Any] = [
             NSAttributedString.Key.foregroundColor: view.tintColor!,
             NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 16)
@@ -25,12 +27,23 @@ class ViewController: UIViewController {
         tv.maximumNumberOfLines = 6
         tv.shouldTrim = true
     }
-
+    
+    
+    @IBAction func btnAction(_ sender: UIButton) {
+       let vc = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "SecondViewController") as! SecondViewController
+                     //        let vc = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(identifier: "SecondViewController") as! SecondViewController
+                         
+                     //vc.modalPresentationStyle = .fullScreen
+                             present(vc, animated: true, completion: nil)
+    }
+    
 }
 
 extension ViewController: TextViewTapActionProtocol {
     func tapAction() {
-        
+       let vc = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "SecondViewController") as! SecondViewController
+  
+        present(vc, animated: true, completion: nil)
     }
     
     
