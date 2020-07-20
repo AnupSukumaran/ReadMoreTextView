23b9567fbcd9fc0ec64dde267e3c7c340907f3c4
Anup Sukumaran <anup.sukumaran9@gmail.com>
2020-07-20 18:39:12 +0530

    - 6:38 pm - fixing the font size for read more

--- ReadMoreText/ViewController.swift
+++ ReadMoreText/ViewController.swift
@@ -19,13 +19,13 @@ class ViewController: UIViewController {
         super.viewDidLoad()
          tv.text = "👨‍👩‍👧‍👦👨‍👩‍👧‍👦👨‍👩‍👧‍👦👨‍👩‍👧‍👦 Lorem http://ipsum.com dolor sit er elit lamet, consectetaur cillium adipisicing pecu, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Nam liber te conscient to factor tum poen legum odioque civiuda."
        // let ss =  UIFont.boldSystemFont(ofSize: 16)
-        let readMoreTextAttributes: [NSAttributedString.Key: Any] = [
-            NSAttributedString.Key.foregroundColor: view.tintColor!,
-            NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 16)
-        ]
-        tv.attributedReadMoreText = NSAttributedString(string: "... Read more", attributes: readMoreTextAttributes)
-        tv.maximumNumberOfLines = 6
-        tv.shouldTrim = true
+//        let readMoreTextAttributes: [NSAttributedString.Key: Any] = [
+//            NSAttributedString.Key.foregroundColor: view.tintColor!,
+//            NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 16)
+//        ]
+//        tv.attributedReadMoreText = NSAttributedString(string: "... Read more", attributes: readMoreTextAttributes)
+//        tv.maximumNumberOfLines = 6
+//        tv.shouldTrim = true
     }
     
     
